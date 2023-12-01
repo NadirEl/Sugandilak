ae13559977e8df7b8389f652d2ef8cf4e38a2e36
UnaiCM <unai.canomo@elorrieta-errekamari.com>
2023-12-01 12:54:04 +0100

    commit2

---
+++ app/src/main/java/com/example/sugandilak/Labriento.java
@@ -0,0 +1,13 @@
+package com.example.sugandilak;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Labriento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_labriento);
+    }
+}
