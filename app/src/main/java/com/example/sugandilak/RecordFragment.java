07552b8330a1a86afcc1a5c8706b041580be31f1
Igorbuenopo <igor.buenopo@elorrieta-errekamari.com>
2024-01-29 10:42:37 +0100

    commit igor recordf

---
+++ app/src/main/java/com/example/sugandilak/RecordFragment.java
@@ -0,0 +1,42 @@
+package com.example.sugandilak;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class RecordFragment extends Fragment {
+
+
+
+    public RecordFragment() {
+        // Required empty public constructor
+    }
+    public static RecordFragment getInstance() {
+        RecordFragment fragment = new RecordFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_record, container, false);
+
+        return v;
+    }
+}
